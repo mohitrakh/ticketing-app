391b93fdc25137ce625849f93b4a9a2b87cb01b7
mohitrakh <mohitrakh01@gmail.com>
2025-07-03 22:28:25 +0530

    add sign-out api route

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,13 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 const router = express.Router();
 
-router.post("/api/users/signout", (req, res) => {
-  res.send("Hi there!");
-});
+router.post(
+  "/api/users/signout",
+  (req: Request, res: Response, next: NextFunction) => {
+    req.session = null;
+    res.send({});
+  }
+);
 
 export { router as signoutRouter };
