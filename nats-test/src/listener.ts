08c698c95e30321211ca2541e888630ae10723aa
mohitrakh <mohitrakh01@gmail.com>
2025-07-18 22:35:02 +0530

    add ack mode in our listener NATS

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,16 +1,28 @@
-import nats from "node-nats-streaming";
+import { randomBytes } from "crypto";
+import nats, { Message } from "node-nats-streaming";
 
-const stan = nats.connect("ticketing", "def", {
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("Connected to NATS");
 
-  // Subscribe to the "ticket:created" subject
-  const subscription = stan.subscribe("ticket:created");
+  const options = stan.subscriptionOptions().setManualAckMode(true);
 
-  subscription.on("message", (msg) => {
-    console.log("Received a message on 'ticket:created'");
+  // Subscribe to the "ticket:created" subject
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "orders-service-queue-group",
+    options
+  );
+
+  subscription.on("message", (msg: Message) => {
+    console.log(
+      "Received a message on 'ticket:created'",
+      msg.getData(),
+      msg.getSequence()
+    );
+    msg.ack();
   });
 });
