a01a1ddca35a05a221ad6f3a45a2424260117d22
mohitrakh <mohitrakh01@gmail.com>
2025-07-20 14:23:01 +0530

    update common package

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,36 +1,23 @@
-import { randomBytes } from "crypto";
-import nats, { Message } from "node-nats-streaming";
+import nats from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+import { TicketCreatedListener } from './events/ticket-created-listener';
 
-const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
-  url: "http://localhost:4222",
+console.clear();
+
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+  url: 'http://localhost:4222',
 });
 
-stan.on("connect", () => {
-  console.log("Connected to NATS");
+stan.on('connect', () => {
+  console.log('Listener connected to NATS');
 
-  const options = stan.subscriptionOptions().setManualAckMode(true);
-
-  const subscription = stan.subscribe(
-    "ticket:created",
-    "orders-service-queue-group",
-    options
-  );
-
-  subscription.on("message", (msg: Message) => {
-    console.log(
-      "Received a message on 'ticket:created'",
-      msg.getData(),
-      msg.getSequence()
-    );
-    msg.ack();
-  });
-
-  // ✅ Handle graceful shutdown
-  stan.on("close", () => {
-    console.log("NATS connection closed");
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
     process.exit();
   });
 
-  process.on("SIGINT", () => stan.close()); // Ctrl+C
-  process.on("SIGTERM", () => stan.close()); // Kubernetes / Docker
+  new TicketCreatedListener(stan).listen();
 });
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,19 +1,23 @@
 import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
+
+console.clear();
 
 const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
-  console.log("Publisher connected to nats");
+stan.on("connect", async () => {
+  console.log("Publisher connected to NATS");
 
-  let data = {
-    id: 123,
-    title: "ticket data",
-    price: 23,
-  };
-
-  stan.publish("ticket:created", JSON.stringify(data), () => {
-    console.log("This is callback is completely optional");
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: "123",
+      title: "concert",
+      price: 20,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
